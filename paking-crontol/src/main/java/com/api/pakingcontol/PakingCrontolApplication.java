abd7c9641918900c785799409f494456d159d4db
marcosjr <marcosjrpb@hotmailcom>
2023-06-23 12:02:13 -0300

    Criação da Pasta e arquivos

---
+++ paking-crontol/src/main/java/com/api/pakingcontol/PakingCrontolApplication.java
@@ -0,0 +1,13 @@
+package com.api.pakingcontol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakingCrontolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PakingCrontolApplication.class, args);
+	}
+
+}
---
+++ paking-crontol/src/test/java/com/api/pakingcontol/PakingCrontolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pakingcontol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakingCrontolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
