8c50e9b9b0ec0a0ac1e398f725feaabf76519eba
marcosjr <marcosjrpb@hotmailcom>
2023-06-27 15:16:43 -0300

    Criação da Paginação

--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,9 +1,11 @@
 package com.api.parkingcontrol.services;
 
-import java.util.List;
+ 
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.api.parkingcontrol.model.ParkingSpotModel;
@@ -41,11 +43,13 @@ public class ParkingSpotService {
 	   return parkingSpotRepository.existsByApartmentAndBlock(block, ap);
    }
 
-public List<ParkingSpotModel>findAll() {
+public Page<ParkingSpotModel>findAll(Pageable pageable) {
 	
-	return parkingSpotRepository.findAll();
+	return parkingSpotRepository.findAll(pageable);
 			
 }
+
+
 
 public Optional<ParkingSpotModel> findById(UUID id) {
 	return parkingSpotRepository.findById(id);
